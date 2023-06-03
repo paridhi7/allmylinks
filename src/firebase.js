a270b6f432ad25d2ddbfd9ce0431671d1541d314
paridhi7 <theparidhi0@gmail.com>
2023-06-03 17:27:36 +0530

    Add .env file, update .gitignore and minor CSS fix

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBihPWewzQ6lkS9F4v7FA_K3ChnaW2E4i4",
-  authDomain: "allmylinks-backend.firebaseapp.com",
-  projectId: "allmylinks-backend",
-  storageBucket: "allmylinks-backend.appspot.com",
-  messagingSenderId: "222035620484",
-  appId: "1:222035620484:web:d50848e394f284081848d1",
-  measurementId: "G-TSGLG0LM44",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
